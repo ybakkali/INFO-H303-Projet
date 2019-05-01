418a589de8bf5fc797835b1fc3300160c58984bd
YahyaBakkali <yahyabakkali@hotmail.com>
2019-05-02 01:34:10 +0200

    create db for xml files

--- initialization.sql
+++ initialization.sql
@@ -1,16 +1,38 @@
-CREATE TABLE `REGISTRED_USERS`
-( `ID` int NOT NULL,
-  `lastName` varchar(30) NOT NULL,
-  `firstName` varchar(30) NOT NULL,
+-- https://dev.mysql.com/doc/refman/5.5/en/load-xml.html
+-- mysql -u yahyabakkali -p scooterDB
+-- mysql> source /home/yahyabakkali/Bureau/INFO-H303/BDD/initialization.sql;
+
+CREATE DATABASE IF NOT EXISTS `scooterDB`;
+
+CREATE TABLE IF NOT EXISTS `REGISTRED_USERS`
+( `ID` int unsigned NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `phone` varchar(35) NOT NULL,
-  `address` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `bankaccount` varchar(16) NOT NULL,
   PRIMARY KEY(`ID`)
 ) engine = innodb;
 
-CREATE TABLE `ANONYME_USERS`
-( `ID` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `USER_ADDRESS`
+( `ID` int unsigned NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `cp` int NOT NULL,
+  `street` varchar(40) NOT NULL,
+  `number` int NOT NULL,
+  PRIMARY KEY(`ID`)
+) engine = innodb;
+
+LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml'
+INTO TABLE `scooterDB`.`REGISTRED_USERS`
+ROWS IDENTIFIED BY '<user>';
+
+LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml'
+INTO TABLE `scooterDB`.`USER_ADDRESS`
+ROWS IDENTIFIED BY '<address>';
+
+CREATE TABLE IF NOT EXISTS `ANONYME_USERS`
+( `ID`  int unsigned NOT NULL,
   `password` varchar(64) NOT NULL,
   `bankaccount` varchar(16) NOT NULL,
   PRIMARY KEY(`ID`)
@@ -18,9 +40,32 @@ CREATE TABLE `ANONYME_USERS`
 
 LOAD XML LOCAL INFILE 'data2019/anonyme_users.xml'
 INTO TABLE `scooterDB`.`ANONYME_USERS`
-ROWS IDENTIFIED BY '<user>'
--- (@Buyer, @Seller, @Time, @Rate)
--- SET `rate`=@Rate , `date`=@Time, `buyerID`=@Buyer, `sellerID`=@Seller;
+ROWS IDENTIFIED BY '<user>';
 
--- mysql -u yahyabakkali -p scooterDB
--- mysql> source /home/yahyabakkali/Bureau/INFO-H303/BDD/initialization.sql;
+CREATE TABLE IF NOT EXISTS `MECANICIENS`
+( `mechanicID` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `hireDate` DATE NOT NULL,
+  `bankaccount` varchar(16) NOT NULL,
+  PRIMARY KEY(`mechanicID`)
+) engine = innodb;
+
+CREATE TABLE IF NOT EXISTS `MECHANIC_ADDRESS`
+( `mechanicID` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `cp` int NOT NULL,
+  `street` varchar(40) NOT NULL,
+  `number` int NOT NULL,
+  PRIMARY KEY(`mechanicID`)
+) engine = innodb;
+
+LOAD XML LOCAL INFILE 'data2019/mecaniciens.xml'
+INTO TABLE `scooterDB`.`MECANICIENS`
+ROWS IDENTIFIED BY '<mechanic>';
+
+LOAD XML LOCAL INFILE 'data2019/mecaniciens.xml'
+INTO TABLE `scooterDB`.`MECHANIC_ADDRESS`
+ROWS IDENTIFIED BY '<address>';
