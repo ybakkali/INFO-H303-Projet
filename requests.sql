a0e96934854c5bc6b277015145bb65bb9df902e5
afallahi97 <amir.m.fallahi@aol.com>
2019-05-03 20:53:25 +0200

    req 1,2,3,4

--- requests.sql
+++ requests.sql
@@ -47,3 +47,37 @@ SET `locationY` =( SELECT `destinationY`
 
 UPDATE `SCOOTERS`
 SET `availability` = 1;
+
+
+/*R1*/
+SELECT s.`scooterID`, s.`locationX`, s.`locationY`
+FROM `SCOOTERS` s;
+
+
+/*R2 (Si c'est: -> La liste des utilisateurs ayant utilisé toutes les trottinettes qu'ils ont rechargées.)*/
+
+SELECT DISTINCT rl.`userID`
+FROM `RELOADS` rl, `TRIPS` tr
+WHERE rl.`userID` = tr.`userID`
+ORDER BY rl.`userID`;
+
+
+/*R3*/
+SELECT tr.`scooterID`
+FROM `TRIPS` tr
+WHERE ((power(power(tr.`destinationY`-tr.`sourceY`,2)+power(tr.`destinationX`-tr.`sourceX`,2),0.5)) =
+       ( SELECT max(power(power(t.`destinationY`-t.`sourceY`,2)+power(t.`destinationX`-t.`sourceX`,2),0.5))
+         FROM `TRIPS` t
+       ));
+
+
+
+/*R4*/
+SELECT DISTINCT rep.`scooterID`
+FROM `REPARATIONS` rep
+WHERE(
+      SELECT count(r.`scooterID`)
+      FROM `REPARATIONS` r
+      WHERE r.`scooterID` = rep.`scooterID`
+      GROUP BY r.`scooterID`
+    ) >= 10;
