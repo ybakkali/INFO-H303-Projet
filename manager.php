aaf04004356eace6228317f6bf9ec2d80438d4c0
afallahi97 <amir.m.fallahi@aol.com>
2019-05-06 18:10:21 +0200

    adding user

--- manager.php
+++ manager.php
@@ -40,23 +40,76 @@ if (!$link) {
     die("ERROR: (Connection to database was unsuccessful!) : " . mysqli_connect_error());
     echo "\n";
 }
-echo "Connection to database was successful. \n";
+//echo "Connection to database was successful. \n";
 
 //example
+/*
 $req = "SELECT scooterID FROM SCOOTERS";
 $result = mysqli_query($link, $req);
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "scooterID: " . $row["scooterID"].  "\n";
+        echo "scooterID: " . $row["scooterID"].  "<br>";
     }
 }
 else {
     echo "There isn't any result for this request.";
 }
+*/
 
+
+function getRegisteredID() {
+    $last_ID_req = "SELECT max(`ID`)
+                    FROM `ALL_USERS`
+                    WHERE `lastname` IS NOT NULL";
+    $result = mysqli_query($GLOBALS['link'], $last_ID_req);
+    if (mysqli_num_rows($result) > 0) {
+            $data = mysqli_fetch_assoc($result);
+    }
+    return $data["max(`ID`)"] + 1;
+}
+
+
+function getUnregisteredID() {
+    $last_ID_req = "SELECT max(`ID`)
+                    FROM `ALL_USERS`
+                    WHERE `lastname` IS NULL";
+    $result = mysqli_query($GLOBALS['link'], $last_ID_req);
+    if (mysqli_num_rows($result) > 0) {
+            $data = mysqli_fetch_assoc($result);
+    }
+    return $data["max(`ID`)"] + 1;
+}
+
+
+function addUnregiteredUser($password, $bankaccount) {
+    $ID = getUnregisteredID();
+    //echo "Your ID is : ", $ID, "\n";
+    $adding = "INSERT INTO `ALL_USERS` (ID, password, bankaccount)
+               VALUES ($ID, '$password', '$bankaccount')";
+    if (!(mysqli_query($GLOBALS['link'], $adding))) {
+        echo "Error : (could not insert new data !) : " . $adding . "<br>" . mysqli_error($GLOBALS['link']). "<br>";
+    }
+    return $ID; // To display for user
+}
+
+
+function addRegiteredUser($password, $bankaccount, $lastname, $firstname, $phone, $adrsCity, $adrsZip, $adrsStreet, $adrsNumber) {
+    $ID = getRegisteredID();
+    //echo "Your ID is : ", $ID, "\n";
+    $adding = "INSERT INTO `ALL_USERS` (ID, password, bankaccount, lastname, firstname, phone)
+               VALUES ($ID, '$password', '$bankaccount', '$lastname', '$firstname', '$phone')";
+    if (!(mysqli_query($GLOBALS['link'], $adding))) {
+        echo "Error : (could not insert new data !) : " . $adding . "<br>" . mysqli_error($GLOBALS['link']),"\n";
+    }
+    $adding_adrs = "INSERT INTO `USER_ADDRESS` (ID, city, cp, street, number)
+                    VALUES ($ID, '$adrsCity', $adrsZip, '$adrsStreet', $adrsNumber)";
+    if (!(mysqli_query($GLOBALS['link'], $adding_adrs))) {
+        echo "Error : (could not insert new data !) : " . $adding_adrs . "<br>" . mysqli_error($GLOBALS['link']),"\n";
+    }
+    return $ID;
+}
+//addUnregiteredUser('785', '2503250325032503');
+addRegiteredUser('qqq', 'BE12123412341234', 'bakkali', 'Yoyo', '45032145', 'Brux', 1050, 'ULB', 25);
 mysqli_close($link);
-
-
-
 ?>
