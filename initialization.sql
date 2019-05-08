d513b13919dd5bd7bc5b91480646a9804774156d
YahyaBakkali <yahyabakkali@hotmail.com>
2019-05-08 20:06:53 +0200

    update db

--- initialization.sql
+++ initialization.sql
@@ -1,6 +1,3 @@
--- (scooter,user,mechanic,@complainTime,@repaireTime)
--- SET complainTime = STR_TO_DATE(@expired_date, '%m/%d/%Y');
-
 /* To run script do :
 
     mysql -u username -p scooterDB
@@ -26,32 +23,18 @@ CREATE TABLE IF NOT EXISTS `ALL_USERS`
   PRIMARY KEY(`ID`)
 ) engine = innodb;
 
-SELECT '<LOAD ALL_USERS TABLE>' AS '';
-LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml' IGNORE
-INTO TABLE `scooterDB`.`ALL_USERS`
-ROWS IDENTIFIED BY '<user>';
-
-LOAD XML LOCAL INFILE 'data2019/anonyme_users.xml' IGNORE
-INTO TABLE `scooterDB`.`ALL_USERS`
-ROWS IDENTIFIED BY '<user>';
-
 SELECT '<CREATE USER_ADDRESS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `USER_ADDRESS`
 ( `ID` int unsigned NOT NULL,
   `city` varchar(30) NOT NULL,
-  `cp` int NOT NULL,
+  `cp` int unsigned NOT NULL,
   `street` varchar(40) NOT NULL,
-  `number` int NOT NULL,
+  `number` int unsigned NOT NULL,
 
   PRIMARY KEY(`ID`),
   CONSTRAINT fk_userID FOREIGN KEY(ID)
       REFERENCES ALL_USERS(ID)
 ) engine = innodb;
-
-SELECT '<LOAD USER_ADDRESS TABLE>' AS '';
-LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml' IGNORE
-INTO TABLE `scooterDB`.`USER_ADDRESS`
-ROWS IDENTIFIED BY '<address>';
 
 SELECT '<CREATE MECANICIENS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `MECANICIENS`
@@ -65,28 +48,18 @@ CREATE TABLE IF NOT EXISTS `MECANICIENS`
   PRIMARY KEY(`mechanicID`)
 ) engine = innodb;
 
-SELECT '<LOAD MECANICIENS TABLE>' AS '';
-LOAD XML LOCAL INFILE 'data2019/mecaniciens.xml' IGNORE
-INTO TABLE `scooterDB`.`MECANICIENS`
-ROWS IDENTIFIED BY '<mechanic>';
-
 SELECT '<CREATE MECHANIC_ADDRESS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `MECHANIC_ADDRESS`
 ( `mechanicID` varchar(30) NOT NULL,
   `city` varchar(30) NOT NULL,
-  `cp` int NOT NULL,
+  `cp` int unsigned NOT NULL,
   `street` varchar(40) NOT NULL,
-  `number` int NOT NULL,
+  `number` int unsigned NOT NULL,
 
   PRIMARY KEY(`mechanicID`),
   CONSTRAINT fk_mechanicID FOREIGN KEY(mechanicID)
       REFERENCES MECANICIENS(mechanicID)
 ) engine = innodb;
-
-SELECT '<LOAD MECHANIC_ADDRESS TABLE>' AS '';
-LOAD XML LOCAL INFILE 'data2019/mecaniciens.xml' IGNORE
-INTO TABLE `scooterDB`.`MECHANIC_ADDRESS`
-ROWS IDENTIFIED BY '<address>';
 
 SELECT '<CREATE SCOOTERS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `SCOOTERS`
@@ -94,22 +67,12 @@ CREATE TABLE IF NOT EXISTS `SCOOTERS`
   `commissioningDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modelNumber` varchar(30) NOT NULL,
   `complainState` boolean NOT NULL DEFAULT False,
-  `batteryLevel` int NOT NULL DEFAULT 4,
+  `batteryLevel` int unsigned NOT NULL DEFAULT 4,
   `locationX` float  NOT NULL DEFAULT 0,
   `locationY` float  NOT NULL DEFAULT 0,
-  `availability` boolean NOT NULL DEFAULT True,
+  `availability` ENUM('available','occupy','inRepair') NOT NULL DEFAULT 'available',
   PRIMARY KEY(`scooterID`)
 ) engine = innodb;
-
-SELECT '<LOAD SCOOTERS TABLE>' AS '';
-LOAD DATA LOCAL INFILE 'data2019/scooters.csv' IGNORE
-INTO TABLE `scooterDB`.`SCOOTERS`
-FIELDS TERMINATED BY ';' ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(`scooterID`,`commissioningDate`,`modelNumber`,@var,`batteryLevel`)
-SET `complainState` = IF(@var = 'False',0,1);
-
 
 SELECT '<CREATE COMPLAINS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `COMPLAINS`
@@ -117,6 +80,7 @@ CREATE TABLE IF NOT EXISTS `COMPLAINS`
   `userID` int unsigned NOT NULL,
   `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `description` varchar(250) NOT NULL DEFAULT '',
+  `state` ENUM('inProcess','notTreated','treated') NOT NULL DEFAULT 'notTreated',
 
   INDEX(`date`),
   PRIMARY KEY(`userID`,`scooterID`,`date`),
@@ -125,14 +89,6 @@ CREATE TABLE IF NOT EXISTS `COMPLAINS`
   CONSTRAINT fk_user_3 FOREIGN KEY(userID)
       REFERENCES ALL_USERS(ID)
 ) engine = innodb;
-
-SELECT '<LOAD COMPLAINS TABLE>' AS '';
-LOAD DATA LOCAL INFILE 'data2019/reparations.csv' IGNORE
-INTO TABLE `scooterDB`.`COMPLAINS`
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(`scooterID`, `userID`, @dummy, `date`, @dummy);
 
 SELECT '<CREATE REPARATIONS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `REPARATIONS`
@@ -149,16 +105,9 @@ CREATE TABLE IF NOT EXISTS `REPARATIONS`
       REFERENCES ALL_USERS(`ID`),
   CONSTRAINT fk_mechanic_1 FOREIGN KEY(`mechanicID`)
       REFERENCES MECANICIENS(`mechanicID`),
-  FOREIGN KEY(`complainTime`)
+  CONSTRAINT fk_complain FOREIGN KEY(`complainTime`)
       REFERENCES COMPLAINS(`date`)
 ) engine = innodb;
-
-SELECT '<LOAD REPARATIONS TABLE>' AS '';
-LOAD DATA LOCAL INFILE 'data2019/reparations.csv' IGNORE
-INTO TABLE `scooterDB`.`REPARATIONS`
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
 
 SELECT '<CREATE RELOADS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `RELOADS`
@@ -180,15 +129,6 @@ CREATE TABLE IF NOT EXISTS `RELOADS`
       REFERENCES ALL_USERS(ID)
 ) engine = innodb;
 
-SELECT '<LOAD RELOADS TABLE>' AS '';
-LOAD DATA LOCAL INFILE 'data2019/reloads.csv' IGNORE
-INTO TABLE `scooterDB`.`RELOADS`
-FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(`scooterID`,`userID`,`initialLoad`,`finalLoad`,@x, @y, @i , @j,`starttime`,`endtime`)
-SET sourceX = @y, sourceY = @x, destinationX = @j, destinationY = @i;
-
 SELECT '<CREATE TRIPS TABLE>' AS '';
 CREATE TABLE IF NOT EXISTS `TRIPS`
 ( `scooterID` int unsigned NOT NULL,
@@ -209,9 +149,102 @@ CREATE TABLE IF NOT EXISTS `TRIPS`
       REFERENCES ALL_USERS(ID)
 ) engine = innodb;
 
+DELIMITER |
+CREATE TRIGGER REPARATIONS_TRIGGER BEFORE INSERT ON `REPARATIONS`
+  FOR EACH ROW
+  BEGIN
+    IF NEW.complainTime > NEW.repaireTime THEN
+      signal sqlstate '45000'  SET MESSAGE_TEXT = 'An error occurred';
+    ELSE
+      UPDATE `COMPLAINS` C
+      SET state = 'treated'
+      WHERE C.`scooterID` = NEW.scooterID AND
+            C.`date`      < NEW.repaireTime;
+    END IF;
+  END |
+
+CREATE TRIGGER RELOADS_TRIGGER BEFORE INSERT ON `RELOADS`
+  FOR EACH ROW
+  BEGIN
+    IF NEW.initialLoad > NEW.finalLoad
+       AND NEW.starttime > NEW.endtime
+       AND TIME(NEW.starttime) NOT BETWEEN '22:00:00' and '07:00:00'
+       AND TIME(NEW.endtime) NOT BETWEEN '22:00:00' and '07:00:00'
+       THEN
+          signal sqlstate '45000'  SET MESSAGE_TEXT = 'An error occurred';
+    END IF;
+  END |
+
+CREATE TRIGGER TRIPS_TRIGGER BEFORE INSERT ON `TRIPS`
+  FOR EACH ROW
+  BEGIN
+    IF NEW.starttime > NEW.endtime THEN
+          signal sqlstate '45000'  SET MESSAGE_TEXT = 'An error occurred';
+    END IF;
+  END |
+
+DELIMITER ;
+
+SELECT '<LOAD ALL_USERS TABLE>' AS '';
+LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml' IGNORE
+INTO TABLE `ALL_USERS`
+ROWS IDENTIFIED BY '<user>';
+
+LOAD XML LOCAL INFILE 'data2019/anonyme_users.xml' IGNORE
+INTO TABLE `ALL_USERS`
+ROWS IDENTIFIED BY '<user>';
+
+SELECT '<LOAD USER_ADDRESS TABLE>' AS '';
+LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml' IGNORE
+INTO TABLE `USER_ADDRESS`
+ROWS IDENTIFIED BY '<address>';
+
+SELECT '<LOAD MECANICIENS TABLE>' AS '';
+LOAD XML LOCAL INFILE 'data2019/mecaniciens.xml' IGNORE
+INTO TABLE `MECANICIENS`
+ROWS IDENTIFIED BY '<mechanic>';
+
+SELECT '<LOAD MECHANIC_ADDRESS TABLE>' AS '';
+LOAD XML LOCAL INFILE 'data2019/mecaniciens.xml' IGNORE
+INTO TABLE `MECHANIC_ADDRESS`
+ROWS IDENTIFIED BY '<address>';
+
+SELECT '<LOAD SCOOTERS TABLE>' AS '';
+LOAD DATA LOCAL INFILE 'data2019/scooters.csv' IGNORE
+INTO TABLE `SCOOTERS`
+FIELDS TERMINATED BY ';' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`scooterID`,`commissioningDate`,`modelNumber`,@var,`batteryLevel`)
+SET `complainState` = IF(@var = 'False',0,1);
+
+SELECT '<LOAD COMPLAINS TABLE>' AS '';
+LOAD DATA LOCAL INFILE 'data2019/reparations.csv' IGNORE
+INTO TABLE `COMPLAINS`
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`scooterID`, `userID`, @dummy, `date`, @dummy);
+
+SELECT '<LOAD REPARATIONS TABLE>' AS '';
+LOAD DATA LOCAL INFILE 'data2019/reparations.csv' IGNORE
+INTO TABLE `REPARATIONS`
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT '<LOAD RELOADS TABLE>' AS '';
+LOAD DATA LOCAL INFILE 'data2019/reloads.csv' IGNORE
+INTO TABLE `RELOADS`
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`scooterID`,`userID`,`initialLoad`,`finalLoad`,@x, @y, @i , @j,`starttime`,`endtime`)
+SET sourceX = @y, sourceY = @x, destinationX = @j, destinationY = @i;
+
 SELECT '<LOAD TRIPS TABLE>' AS '';
 LOAD DATA LOCAL INFILE 'data2019/trips.csv' IGNORE
-INTO TABLE `scooterDB`.`TRIPS`
+INTO TABLE `TRIPS`
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
@@ -247,11 +280,11 @@ CREATE TABLE IF NOT EXISTS `REGISTRED_ADDRESS`
 ) engine = innodb;
 
 LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml'
-INTO TABLE `scooterDB`.`REGISTRED_USERS`
+INTO TABLE `REGISTRED_USERS`
 ROWS IDENTIFIED BY '<user>';
 
 LOAD XML LOCAL INFILE 'data2019/registeredUsers.xml'
-INTO TABLE `scooterDB`.`REGISTRED_ADDRESS`
+INTO TABLE `REGISTRED_ADDRESS`
 ROWS IDENTIFIED BY '<address>';
 
 CREATE TABLE IF NOT EXISTS `ANONYME_USERS`
@@ -262,7 +295,7 @@ CREATE TABLE IF NOT EXISTS `ANONYME_USERS`
 ) engine = innodb;
 
 LOAD XML LOCAL INFILE 'data2019/anonyme_users.xml'
-INTO TABLE `scooterDB`.`ANONYME_USERS`
+INTO TABLE `ANONYME_USERS`
 ROWS IDENTIFIED BY '<user>';
 
 CREATE TABLE IF NOT EXISTS `ALL_USERS`
@@ -293,17 +326,17 @@ WHERE (`scooterID`,`endtime`) IN (SELECT `scooterID`, max(`endtime`)
                                GROUP BY `scooterID`
                                ORDER BY `scooterID`
                              );
-                             UPDATE `SCOOTERS` S
-                             SET `locationX` =( SELECT `destinationX`
-                                                FROM `TRIPS` T
-                                                WHERE T.`scooterID` = S.`scooterID`
-                                                      AND (`scooterID`,`endtime`) IN
-                                                          ( SELECT `scooterID`, max(`endtime`)
-                                                            FROM `TRIPS`
-                                                            GROUP BY `scooterID`
-                                                            ORDER BY `scooterID`
-                                                          )
-                                               );
+UPDATE `SCOOTERS` S
+SET `locationX` =( SELECT `destinationX`
+                  FROM `TRIPS` T
+                  WHERE T.`scooterID` = S.`scooterID`
+                        AND (`scooterID`,`endtime`) IN
+                            ( SELECT `scooterID`, max(`endtime`)
+                              FROM `TRIPS`
+                              GROUP BY `scooterID`
+                              ORDER BY `scooterID`
+                            )
+                 );
 
 UPDATE `SCOOTERS` S
 SET `locationY` =( SELECT `destinationY`
@@ -316,10 +349,4 @@ SET `locationY` =( SELECT `destinationY`
                               ORDER BY `scooterID`
                             )
                  );
-
-(CASE
-WHEN HOUR(`duration`) = 0 THEN 1 + (MINUTE(`duration`) * 0.15) / 100
-WHEN HOUR(`duration`) BETWEEN 1 AND 23 THEN 1 + HOUR(`duration`) * 6,5 + (MINUTE(`duration`) * 0.15) / 100
-ELSE 1 + HOUR(`duration`) DIV 24 * 36 +  HOUR(`duration`) % 24 * 6.5 + (MINUTE(`duration`) * 0.15) / 100
-END)
 */
